0e96ce769ef2a0c4401fbc9a397c13d17925f1fc
arnaudmanaranche <hello@enaut.dev>
2026-06-17 11:58:42 +0200

    feat: add components/variant-split rule for v0.2

--- packages/cli/src/core/rules/components/index.ts
+++ packages/cli/src/core/rules/components/index.ts
@@ -1,11 +1,14 @@
 import { Rule } from '../../types';
 import { deadComponentsRule } from './dead/rule';
 import { duplicatesRule } from './duplicates/rule';
+import { variantSplitRule } from './variant-split/rule';
 
 export const componentRules: Rule[] = [
   deadComponentsRule,
   duplicatesRule,
+  variantSplitRule,
 ];
 
 export { deadComponentsRule } from './dead/rule';
 export { duplicatesRule } from './duplicates/rule';
+export { variantSplitRule } from './variant-split/rule';
